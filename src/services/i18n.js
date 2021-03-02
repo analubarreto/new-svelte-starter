cf34d5d6c78746791a7146215a9eac9c3182ba27
analubarreto <analuibm@gmail.com>
2021-03-02 16:11:09 -0300

    added json lang files to public folder

--- src/services/i18n.js
+++ src/services/i18n.js
@@ -1,37 +1,48 @@
-// Add i18n configuration
-import { dictionary, locale, _ } from 'svelte-i18n';
-
-function setupI18n({ withLocale: _locale} = { withLocale: 'en' }) {
-  dictionary.set({
-    en: {
-      'home': { 'exampleText': 'Just an example text to show i18n working' },
-      'navbar': {
-        'home': 'Home',
-        'about': 'About',
-        'blog': 'Blog'
-      }
-    },
-    pt: {
-      'home': { 'exampleText': 'Só um texto de exemplo para mostrar o i18n funcionando' },
-      'navbar': {
-        'home': 'Início',
-        'about': 'Sobre',
-        'blog': 'Blog'
-      }
-    },
-    es: {
-      'home': { 'exampleText': 'Solo un texto de ejemplo para mostrar i18n funcionando' },
-      'navbar': {
-        'home': 'Home',
-        'about': 'Sobre',
-        'blog': 'Blog'
-      }
-    }
-  });
-  locale.set(_locale);
-}
-
-export {
+import { get } from "svelte/store";
+import {
+  addMessages,
+  locale,
+  init,
+  dictionary,
   _,
-  setupI18n
+} from "svelte-i18n";
+
+const MESSAGE_FILE_URL_TEMPLATE = "/lang/{locale}.json";
+
+function setupI18n(options) {
+  const { withLocale: locale_ } = options;
+
+  // Initialize svelte-i18n
+  init({ initialLocale: locale_ });
+
+  // Don't re-download translation files
+  if (!hasLoadedLocale(locale_)) {
+    const messagesFileUrl = 
+      MESSAGE_FILE_URL_TEMPLATE.replace(
+        "{locale}",
+        locale_,
+      );
+
+    // Download translation file for given locale/language
+    return loadJson(messagesFileUrl).then((messages) => {
+      // Configure svelte-i18n to use the locale
+      addMessages(locale_, messages);
+
+      locale.set(locale_);
+    });
+  }
 }
+
+function loadJson(url) {
+  return fetch(url).then((response) => response.json());
+}
+
+function hasLoadedLocale(locale) {
+  // If the svelte-i18n dictionary has an entry for the
+  // locale, then the locale has already been added
+  return get(dictionary)[locale];
+}
+
+// We expose the svelte-i18n _ store so that our app has
+// a single API for i18n
+export { _, setupI18n };
