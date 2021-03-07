0172f5fac84d27434f62cd53fabef83b090e3452
analubarreto <analuibm@gmail.com>
2021-03-07 14:55:35 -0300

    removed extra file

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "stories": [
+    '../src/components/**/*.stories.js',
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
+  "addons": [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-knobs',
+    '@storybook/addon-storysource'
+  ]
+}
